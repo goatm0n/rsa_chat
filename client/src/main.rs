6e5d54f053743ddc6202fb41ad38d938db9d1a5b
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2022-09-13 17:06:55 +0100

    current dev state

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,188 +1,126 @@
-use std::collections::HashMap;
-use std::error::Error;
-use std::io::{Write, Read};
-use std::path::PathBuf;
-use std::str::from_utf8;
 use clap::Parser; 
-use rsa_rs::encryption::encrypt::encrypt_string;
-use rsa_rs::keys::keypair::*;
-use rsa_rs::encryption::decrypt::decrypt_string;
-use std::net::TcpStream;
-
-
+ 
 #[derive(Parser)]
 struct Cli {
     url: String,
-    //key_path: PathBuf,
 }
 
-struct Message {
-    text: String,
-}
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let args = Cli::parse();
+    let base_url = args.url;
+    let url = base_url + "/messages";
 
-impl Message {
-    fn as_str(&self) -> &str {
-        self.text.as_str()
-    }
+    let new_msg = models::Message {text: "this is the second new message".into()};
+    let new_msg = reqwest::Client::new()
+        .post(url)
+        .json(&new_msg)
+        .send()
+        .await?;
 
-    fn text(&self) -> String {
-        self.text.clone()
-    }
+    let res = reqwest::get(url).await?;
+    let res_text = res.text().await?;
 
-    fn encrypt(&self, public_key: &PublicKey) -> EncryptedMessage {
-        let enc_vec = encrypt_string(&self.text, public_key);
-        let e = public_key.public_exponent_clone();
-        let n = public_key.modulus_clone();
-        let key = PublicKey { public_exponent: e, modulus: n };
-        EncryptedMessage { message: enc_vec, public_key :key }
-    }
-}
-
-struct EncryptedMessage {
-    message: Vec<u128>,
-    public_key: PublicKey,
-}
-
-impl EncryptedMessage {
-    fn from(message: Message, public_key: &PublicKey) -> EncryptedMessage {
-        message.encrypt(public_key)
-    }
-
-    fn to_string(&self) -> String {
-        vec_u128_to_string(&self.message)
-    }
-}
-
-/// clear the terminal screen
-fn cls() {
-    print!("{}[2J", 27 as char);
-}
-
-fn display_tui(msg_list: &Vec<Message>) {
-    //cls();
-    for message in msg_list {
-        let text = message.as_str();
-        println!("{text}");
-    }
-}
-
-fn read_input() -> String {
-    let mut buf = String::new();
-    println!("Enter to refresh: ");
-    std::io::stdin().read_line(&mut buf).unwrap();
-    return buf;
-}
-
-fn write_encrypted_message_to_file(message: Message, public_key: &PublicKey, url: &String, path: &String) {
-    let encrypted_message = message.encrypt(public_key);
-    std::fs::write(path.as_str(), encrypted_message.to_string()).expect("Error writing to outfile.txt");
-}
-
-fn vec_u128_to_string(data: &Vec<u128>) -> String {
-    let mut s = String::new();
-    for num in data {
-        let num_string = num.to_string();
-        s += num_string.as_str();
-        s += "\t";
-    }
-    return s;
-}
-
-fn post_encrypted_message(url: &String) -> Result<(), Box<dyn Error>> {
-    let file = std::fs::File::open("outfile.txt").expect("could not open outfile.txt");
-
-    let body = reqwest::blocking::Body::new(file);
-
-    let client = reqwest::blocking::Client::new();
+    dbg!(res_text);
     
-    let res = client.post(url).body(body).send()?; 
-    dbg!(&res);
     Ok(())
 }
 
-fn get_data(url: &String) -> Vec<Vec<u128>> {
-    let body = reqwest::blocking::get(url).expect("Error making get request")
-        .text().expect("Error getting text");
-    to_vec_vec_u128(&body)
-
-}
-
-fn to_vec_vec_u128(data: &String) -> Vec<Vec<u128>> {
-    let mut vec_vec_u128: Vec<Vec<u128>> = Vec::new();
-    let mut vec_string = String::new();
-    for c in data.chars() {
-        if c != '\n' {
-            vec_string.push(c);
-        } else {
-            let vec_u128 = to_vec_u128(&vec_string);
-            vec_vec_u128.push(vec_u128);
-            vec_string.clear();
-
+mod convert {
+    
+    pub fn vec_u128_to_string(data: &Vec<u128>) -> String {
+        let mut s = String::new();
+        for num in data {
+            let num_string = num.to_string();
+            s += num_string.as_str();
+            s += "\t";
         }
+        return s;
     }
-    return vec_vec_u128;
-}
 
-fn to_vec_u128(data: &String) -> Vec<u128> {
-    let mut vec_u128: Vec<u128> = Vec::new();
-    let mut num_string: String = String::new();
-    for c in data.chars() {
-        if c != '\t' {
-            num_string.push(c);
-        } else {
-            let num: u128 = num_string.parse().expect("error parsing string to u128");
-            vec_u128.push(num);
-            num_string.clear();
-
-        }
-    }
-    return vec_u128;
-}
-
-fn decrypt_data(data: &Vec<Vec<u128>>, private_key: &PrivateKey) -> Vec<String> {
-    let mut decrypted_string_vec: Vec<String> = Vec::new();
-    for enc_vec in data {
-        let decrypted_string = decrypt_string(enc_vec, private_key);
-        decrypted_string_vec.push(decrypted_string);
-    }
-    return decrypted_string_vec;
-}
-
-fn main() {
-    let args = Cli::parse();
-    let base_url = args.url;
-
-    match TcpStream::connect(&base_url) {
-        Ok(mut stream) => {
-            println!("Succesfully connected to {}", base_url);
-
-            let msg = b"Hello!";
-
-            stream.write(msg).unwrap();
-            println!("Sent Hello, awaiting reply...");
-
-            let mut data = [0 as u8; 6]; // using a 6 byte buffer
-            match stream.read_exact(&mut data) {
-                Ok(_) => {
-                    if &data == msg {
-                        println!("Reply is ok!");
-                    } else {
-                        let text = from_utf8(&data).unwrap();
-                        println!("Unexpected reply: {}", text);
-                    }
-                },
-                Err(e) => {
-                    println!("Failed to receive data: {}", e);
-                }
+    pub fn to_vec_vec_u128(data: &String) -> Vec<Vec<u128>> {
+        let mut vec_vec_u128: Vec<Vec<u128>> = Vec::new();
+        let mut vec_string = String::new();
+        for c in data.chars() {
+            if c != '\n' {
+                vec_string.push(c);
+            } else {
+                let vec_u128 = to_vec_u128(&vec_string);
+                vec_vec_u128.push(vec_u128);
+                vec_string.clear();
             }
-            
-        },
-        Err(e) => {
-            println!("Failed to connect: {}", e);
+        }
+        return vec_vec_u128;
+    }
+
+    pub fn to_vec_u128(data: &String) -> Vec<u128> {
+        let mut vec_u128: Vec<u128> = Vec::new();
+        let mut num_string: String = String::new();
+        for c in data.chars() {
+            if c != '\t' {
+                num_string.push(c);
+            } else {
+                let num: u128 = num_string.parse().expect("error parsing string to u128");
+                vec_u128.push(num);
+                num_string.clear();
+            }
+        }
+        return vec_u128;
+    }
+}
+
+
+mod models {
+    use rsa_rs::encryption::encrypt::encrypt_string;
+    use rsa_rs::encryption::decrypt::decrypt_string;
+    use rsa_rs::keys::keypair::{PublicKey, PrivateKey};
+    use serde::{Deserialize, Serialize};
+    use super::convert;
+
+
+    #[derive(Debug, Deserialize, Serialize)]
+    pub struct Message {
+        pub text: String,
+    }
+
+    #[derive(Debug, Deserialize, Serialize)]
+    pub struct EncryptedMessage {
+        pub message: Vec<u128>,
+    }
+
+    impl Message {
+        pub fn encrypt(&self, public_key: &PublicKey) -> EncryptedMessage {
+            EncryptedMessage { message: encrypt_string(&self.text, public_key) }
         }
     }
 
-    println!("Terminated.");
+    impl EncryptedMessage {
+        pub fn decrypt(&self, private_key: &PrivateKey) -> Message {
+            Message { text: decrypt_string(&self.message, private_key) }
+        }
+        pub fn to_string(&self) -> String {
+            convert::vec_u128_to_string(&self.message)
+        }
+    }
 }
+
+
+mod tui {
+
+    /// clear the terminal screen
+    pub fn cls() {
+        print!("{}[2J", 27 as char);
+    }
+
+    pub fn read_input() -> String {
+        let mut buf = String::new();
+        println!("Enter to refresh: ");
+        std::io::stdin().read_line(&mut buf).unwrap();
+        return buf;
+    }
+}
+
+
+
 
 
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,40 +1,128 @@
-use std::{
-    io::{Read, Write},
-    net::{TcpListener, TcpStream},
-};
-use gthread::ThreadPool;
+use warp::Filter;
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer: [u8; 1024] = [0; 1024];
-    while match stream.read(&mut buffer) {
-        Ok(size) => {
-            stream.write(&buffer[0..size]).unwrap();
-            true
-        },
-        Err(_) => {
-            println!("Error; Terminating connection with {}", stream.peer_addr().unwrap());
-            stream.shutdown(std::net::Shutdown::Both).unwrap();
-            false
-        }
-    } {}
+
+#[tokio::main]
+async fn main() {
+    let db = models::blank_db();
+    let api = filters::messages(db);
+    let routes = api.with(warp::log("messages"));
+    warp::serve(routes).run(([127, 0, 0, 1], 6969)).await;
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    println!("Server listening on port 7878");
-    let pool = ThreadPool::new(4);
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("New connection: {}", stream.peer_addr().unwrap());
-                pool.execute(|| {
-                    handle_connection(stream);
-                });
-            }
-            Err(e) => {
-                println!("Error: {}", e)
-            }
+mod filters {
+    use super::handlers;
+    use super::models::{Db, Message};
+    use warp::Filter;
+
+    pub fn messages(db: Db) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+        messages_list(db.clone())
+            .or(messages_create(db.clone()))
+    }
+
+    /// GET /messages 
+    pub fn messages_list(db: Db) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+        warp::path!("messages")
+            .and(warp::get())
+            .and(with_db(db))
+            .and_then(handlers::list_messages)
+    }
+
+    /// POST /messages with json body
+    pub fn messages_create(db: Db) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+        warp::path!("messages")
+            .and(warp::post())
+            .and(json_body())
+            .and(with_db(db))
+            .and_then(handlers::create_message)
+    }
+
+    fn with_db(db: Db) -> impl Filter<Extract = (Db,), Error = std::convert::Infallible> + Clone {
+        warp::any().map(move || db.clone())
+    }
+
+    fn json_body() -> impl Filter<Extract = (Message,), Error = warp::Rejection> + Clone {
+        // when accepting a body, we want a json body
+        // (and to reject huge payloads)...
+        warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+    }
+
+}
+
+mod handlers {
+    use super::models::{Db, Message};
+    use std::convert::Infallible;
+    use warp::http::StatusCode;
+
+    pub async fn list_messages(db: Db) -> Result<impl warp::Reply, Infallible> {
+        let messages = db.lock().await;
+        let messages: Vec<Message> = messages
+            .clone()
+            .into_iter()
+            .collect();
+        Ok(warp::reply::json(&messages))
+    }
+
+    pub async fn create_message(message: Message, db: Db) -> Result<impl warp::Reply, Infallible> {
+        let mut vec = db.lock().await;
+
+        vec.push(message);
+
+        Ok(StatusCode::CREATED)
+    }
+}
+
+mod models {
+    use serde::{Deserialize, Serialize};
+    use std::sync::Arc;
+    use tokio::sync::Mutex;
+
+    pub type Db = Arc<Mutex<Vec<Message>>>;
+
+    pub fn blank_db() -> Db {
+        Arc::new(Mutex::new(Vec::new()))
+    }
+    
+    #[derive(Debug, Deserialize, Serialize, Clone)]
+    pub struct Message {
+        pub text: String,
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use warp::http::StatusCode;
+    use warp::test::request;
+    use super::filters;
+    use super::models::{blank_db, Message};
+
+    #[tokio::test]
+    async fn test_post() {
+        let db = blank_db();
+        let api = filters::messages(db);
+
+        let resp = request()
+            .method("POST")
+            .path("/messages")
+            .json(&message1())
+            .reply(&api)
+            .await;
+
+        assert_eq!(resp.status(), StatusCode::CREATED);
+    }
+
+    fn message1() -> Message {
+        Message {
+            text: "This is the message".into(),
         }
     }
-    println!("Shutting down.");
+
 }
+
+
+
+
+
+
+
+
